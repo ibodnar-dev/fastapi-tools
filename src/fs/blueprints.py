a41dcc92b27bb698fc91edca465d9b0af8090a58
ivan <ibodnar.dev@gmail.com>
2022-08-11 07:30:11 -0300

    blueprint.py created

---
+++ src/fs/blueprints.py
@@ -0,0 +1,16 @@
+app = {
+    'users': [
+        '__init__.py',
+        {
+            'domain': [
+                '__init__.py',
+                {
+                    'services': [
+                        '__init__.py',
+                        'repositories.py'
+                    ]
+                }
+            ]
+        }
+    ]
+}
