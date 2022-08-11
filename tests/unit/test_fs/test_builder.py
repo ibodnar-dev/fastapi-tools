3c868bc789e9f52b78fbab8a68680bf432e8619b
ivan <ibodnar.dev@gmail.com>
2022-08-11 20:44:43 -0300

    test_instantiates_with_arguments created

--- tests/unit/test_fs/test_builder.py
+++ tests/unit/test_fs/test_builder.py
@@ -1,3 +1,16 @@
+from unittest.mock import Mock
+
+from src.fs.builder import Node
+
 
 class TestNode:
-    pass
+    def test_instantiates_with_arguments(self):
+        name = 'name'
+        parent = Mock()
+        is_directory = True
+
+        node = Node(name=name, parent=parent, is_directory=is_directory)
+
+        assert node.name == name
+        assert node.parent == parent
+        assert node.is_directory == is_directory
