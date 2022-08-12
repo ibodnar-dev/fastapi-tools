56cde95b8264c5f3dfaf892f90434392ef59e9c1
ivan <ibodnar.dev@gmail.com>
2022-08-11 21:00:13 -0300

    test_is_root_returns_correct_result created

--- tests/unit/test_fs/test_builder.py
+++ tests/unit/test_fs/test_builder.py
@@ -1,5 +1,7 @@
 from unittest.mock import Mock
 
+import pytest
+
 from src.fs.builder import Node
 
 
@@ -14,3 +16,9 @@ class TestNode:
         assert node.name == name
         assert node.parent == parent
         assert node.is_directory == is_directory
+
+    @pytest.mark.parametrize('parent, is_root_result', [(Mock(spec=Node), False), (None, True)])
+    def test_is_root_returns_correct_result(self, parent, is_root_result):
+        node = Node(name='name', parent=parent)
+
+        assert node.is_root == is_root_result
