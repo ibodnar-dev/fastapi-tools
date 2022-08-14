2132aab3f10119ea64db22734a25ee27f978117a
ivan <ibodnar.dev@gmail.com>
2022-08-14 18:16:49 -0300

    test_creates_path_object_from_path_attribute created

--- tests/unit/test_fs/test_writer.py
+++ tests/unit/test_fs/test_writer.py
@@ -1,3 +1,17 @@
+from unittest.mock import patch, MagicMock
+
+import src.fs.writer as writer_module
+from src.fs.builder import Node
+from src.fs.writer import tree_writer
+
 
 class TestTreeWriter:
-    pass
+    @patch.object(writer_module, 'Path')
+    def test_creates_path_object_from_path_attribute(self, path_mock: MagicMock, nodes_list: list[Node]):
+        directory, file = nodes_list
+
+        tree_writer(tree=nodes_list)
+
+        path_mock.assert_any_call(directory.path)
+        path_mock.assert_any_call(file.path)
+        assert path_mock.call_count == 2
