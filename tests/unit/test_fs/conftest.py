00ba27336e940703da61ba3b51e17b13414eedf7
ivan <ibodnar.dev@gmail.com>
2022-08-14 10:42:41 -0300

    blueprint_and_tree_example_one fixture created

---
+++ tests/unit/test_fs/conftest.py
@@ -0,0 +1,35 @@
+from collections import namedtuple
+
+import pytest
+
+from src.fs.builder import Node
+
+examples_result = namedtuple('examples_result', ['blueprint', 'tree'])
+
+
+@pytest.fixture
+def blueprint_and_tree_example_one() -> examples_result:
+    blueprint = {
+        'base': [
+            'file_one',
+            {
+                'directory_one': [
+                    'file_two'
+                ]
+            }
+        ]
+    }
+
+    base_node = Node(name='base', parent=None, is_directory=True)
+    file_one_node = Node(name='file_one', parent=base_node)
+    directory_one_node = Node(name='directory_one', parent=base_node, is_directory=True)
+    file_two_node = Node(name='file_two', parent=directory_one_node)
+
+    tree = [
+        base_node,
+        file_one_node,
+        directory_one_node,
+        file_two_node
+    ]
+
+    return examples_result(blueprint=blueprint, tree=tree)
