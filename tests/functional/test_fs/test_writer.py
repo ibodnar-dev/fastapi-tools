608af28bfc7a058979955cdaedcf8daaa01b66fe
ivan <ibodnar.dev@gmail.com>
2022-08-15 18:11:12 -0300

    test_raises_when_directory_already_exists created

--- tests/functional/test_fs/test_writer.py
+++ tests/functional/test_fs/test_writer.py
@@ -1,6 +1,9 @@
 from pathlib import Path
 
+import pytest
+
 from src.fs.builder import Node
+from src.fs.exceptions import NodeExistsError
 from src.fs.writer import tree_writer
 
 
@@ -13,3 +16,16 @@ class TestWriter:
 
         assert (tmp_path / base.name).exists()
         assert (tmp_path / base.name / directory_one.name).exists()
+
+    def test_raises_when_directory_already_exists(self, setup_path: Path, simple_tree: list[Node]):
+        base: Node
+        directory_one: Node
+        base, directory_one = simple_tree
+        tmp_path = setup_path
+
+        Path(tmp_path / base.name).mkdir()
+
+        with pytest.raises(NodeExistsError) as exc_info:
+            tree_writer(simple_tree)
+
+        assert 'FileExistsError' in str(exc_info.value)
