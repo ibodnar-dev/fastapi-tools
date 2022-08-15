7e53091ecda12e33d1b5b09ee93744c8a9a002a7
ivan <ibodnar.dev@gmail.com>
2022-08-15 18:10:21 -0300

    BaseFSError str updated

--- src/fs/exceptions.py
+++ src/fs/exceptions.py
@@ -8,8 +8,8 @@ class BaseFSError(Exception):
         message_title = '[message]: '
         return (
             f'{message_title if self.message else ""}{self.message}'
-            f'- [original exception]: {type(self.original_exception).__name__}'
-            f' - [original message]: {self.original_exception.args[1]}'
+            f' [original exception]: {type(self.original_exception).__name__}'
+            f' [original message]: {self.original_exception.args[1]}'
         )
 
 
