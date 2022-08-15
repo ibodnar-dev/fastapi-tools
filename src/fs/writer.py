fc8fccba3a13ef3d356b9fc12466e57ef0bc2900
ivan <ibodnar.dev@gmail.com>
2022-08-15 18:10:51 -0300

    tree_writer: try except added

--- src/fs/writer.py
+++ src/fs/writer.py
@@ -1,12 +1,16 @@
 from pathlib import Path
 
 from src.fs.builder import Node
+from src.fs.exceptions import NodeExistsError
 
 
 def tree_writer(tree: list[Node]):
     for node in tree:
         path_object = Path(node.path)
-        if node.is_directory:
-            path_object.mkdir()
-        else:
-            path_object.touch()
+        try:
+            if node.is_directory:
+                path_object.mkdir()
+            else:
+                path_object.touch()
+        except FileExistsError as e:
+            raise NodeExistsError(e)
