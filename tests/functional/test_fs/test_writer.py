77c9a087c4eb5fcbe5e030ee55578b07af942088
ivan <ibodnar.dev@gmail.com>
2022-08-15 08:39:26 -0300

    test_writes_tree created

--- tests/functional/test_fs/test_writer.py
+++ tests/functional/test_fs/test_writer.py
@@ -1,2 +1,16 @@
+import os
+from pathlib import Path
+
+from src.fs.builder import Node
+from src.fs.writer import tree_writer
+
+
 class TestWriter:
-    pass
+    def test_writes_tree(self, tmp_path: Path, simple_tree: list[Node]):
+        os.chdir(tmp_path)
+        base, directory_one = simple_tree
+
+        tree_writer(simple_tree)
+
+        assert (tmp_path / base.name).exists()
+        assert (tmp_path / base.name / directory_one.name).exists()
