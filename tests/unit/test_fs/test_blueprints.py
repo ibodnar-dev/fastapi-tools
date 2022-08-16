8d29b16143a03791d2e0f8f0058b751915e1565c
ivan <ibodnar.dev@gmail.com>
2022-08-16 08:11:13 -0300

    test_returns_passed_app_name created

--- tests/unit/test_fs/test_blueprints.py
+++ tests/unit/test_fs/test_blueprints.py
@@ -6,3 +6,9 @@ class TestAppBlueprint:
         result = app_blueprint(app_name='app')
 
         assert isinstance(result, dict)
+
+    def test_returns_passed_app_name(self):
+        app_name = 'app'
+        result = app_blueprint(app_name=app_name)
+
+        assert app_name in result
