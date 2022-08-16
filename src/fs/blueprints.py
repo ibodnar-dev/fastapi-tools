e9d0a52982041a08bbcb63bb1e11b602bc3e47bb
ivan <ibodnar.dev@gmail.com>
2022-08-16 07:35:39 -0300

    app_blueprint created

--- src/fs/blueprints.py
+++ src/fs/blueprints.py
@@ -1,16 +1,32 @@
-app = {
-    'users': [
-        '__init__.py',
-        {
-            'domain': [
-                '__init__.py',
-                {
-                    'services': [
-                        '__init__.py',
-                        'repositories.py'
-                    ]
-                }
-            ]
-        }
-    ]
-}
+INIT = '__init__.py'
+
+
+def app_blueprint(app_name: str) -> dict:
+    """
+    Returns a dict suitable to act as a blueprint
+    to create a fs tree for an app.
+    """
+    return {
+        app_name: [
+            INIT,
+            {
+                'domain': [
+                    INIT,
+                    'models.py',
+                    {
+                        'services': [
+                            INIT,
+                        ]
+                    }
+                ]
+            },
+            {
+                'adapters': [
+                    INIT,
+                    'orm.py',
+                    'repositories.py',
+                    'schemas.py'
+                ]
+            }
+        ]
+    }
