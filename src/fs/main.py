923903380a980b369b539d1e01579281f06fb3bd
ivan <ibodnar.dev@gmail.com>
2022-08-17 17:27:07 -0300

    create_app fn implemented

--- src/fs/main.py
+++ src/fs/main.py
@@ -1,2 +1,9 @@
+from src.fs.blueprints import app_blueprint
+from src.fs.builder import build_tree
+from src.fs.writer import write_tree
+
+
 def create_app(app_name: str) -> None:
-    pass
+    blueprint = app_blueprint(app_name=app_name)
+    tree = build_tree(blueprint=blueprint)
+    write_tree(tree=tree)
