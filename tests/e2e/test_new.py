b0272d6bfa0fc888fa99b938b82ce847dee2d333
ivan <ibodnar.dev@gmail.com>
2022-08-19 08:12:30 -0300

    test_creates_file_tree_in_cwd_if_no_path_passed created

--- tests/e2e/test_new.py
+++ tests/e2e/test_new.py
@@ -1,3 +1,5 @@
+import os
+
 from typer.testing import CliRunner
 
 from cli.main import app
@@ -13,3 +15,13 @@ def test_creates_file_tree_in_passed_path(testing_directory):
 
     assert result.exit_code == 0
     assert (testing_directory / app_name).exists()
+
+
+def test_creates_file_tree_in_cwd_if_no_path_passed(tmp_path):
+    os.chdir(tmp_path)
+    app_name = 'users'
+
+    result = runner.invoke(app, ['new', 'app', app_name])
+
+    assert result.exit_code == 0
+    assert (tmp_path / app_name).exists()
