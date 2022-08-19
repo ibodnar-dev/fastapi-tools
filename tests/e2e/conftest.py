b97f0786099dc50decad35ae84eb7071bfd6ad4d
ivan <ibodnar.dev@gmail.com>
2022-08-19 08:00:20 -0300

    testing_directory fixture created

---
+++ tests/e2e/conftest.py
@@ -0,0 +1,13 @@
+from pathlib import Path
+
+import pytest
+
+
+@pytest.fixture(scope='session')
+def testing_directory(tmp_path_factory) -> Path:
+    """
+    Returns a temporary session-scoped directory
+    to share among all the tests in the session,
+    in order to test name collisions and such.
+    """
+    return tmp_path_factory.mktemp('testing_directory')
