bdedb1e50a81f0d572427e7e3b1a72d000be9eca
ivan <ibodnar.dev@gmail.com>
2022-08-19 08:00:33 -0300

    test_creates_file_tree_in_passed_path created

--- tests/e2e/test_new.py
+++ tests/e2e/test_new.py
@@ -1,3 +1,15 @@
 from typer.testing import CliRunner
 
+from cli.main import app
+
+
 runner = CliRunner()
+
+
+def test_creates_file_tree_in_passed_path(testing_directory):
+    app_name = 'users'
+
+    result = runner.invoke(app, ['new', 'app', app_name, '--where', str(testing_directory)])
+
+    assert result.exit_code == 0
+    assert (testing_directory / app_name).exists()
