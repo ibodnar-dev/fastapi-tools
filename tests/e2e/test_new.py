1ccd597672ff51a2bfa029c98023b7f227c61204
ivan <ibodnar.dev@gmail.com>
2022-08-19 08:25:59 -0300

    conftest.py -> session_directory fixture deleted

--- tests/e2e/test_new.py
+++ tests/e2e/test_new.py
@@ -8,13 +8,13 @@ from cli.main import app
 runner = CliRunner()
 
 
-def test_creates_file_tree_in_passed_path(session_directory):
+def test_creates_file_tree_in_passed_path(tmp_path):
     app_name = 'users'
 
-    result = runner.invoke(app, ['new', 'app', app_name, '--where', str(session_directory)])
+    result = runner.invoke(app, ['new', 'app', app_name, '--where', str(tmp_path)])
 
     assert result.exit_code == 0
-    assert (session_directory / app_name).exists()
+    assert (tmp_path / app_name).exists()
 
 
 def test_creates_file_tree_in_cwd_if_no_path_passed(tmp_path):
