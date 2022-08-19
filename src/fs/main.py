c2013767a848eaa68d27778e08c561ea3dde29b1
ivan <ibodnar.dev@gmail.com>
2022-08-18 21:08:10 -0300

    create_app: base_path arg added

--- src/fs/main.py
+++ src/fs/main.py
@@ -3,7 +3,7 @@ from src.fs.builder import build_tree
 from src.fs.writer import write_tree
 
 
-def create_app(app_name: str) -> None:
+def create_app(app_name: str, base_path: str | None = None) -> None:
     blueprint = app_blueprint(app_name=app_name)
     tree = build_tree(blueprint=blueprint)
-    write_tree(tree=tree)
+    write_tree(tree=tree, base_path=base_path)
