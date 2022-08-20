6e933576ff6071be550a37528fab64ee35c3413c
ivan <ibodnar.dev@gmail.com>
2022-08-20 09:37:17 -0300

    test_fails_when_directory_exists created

--- tests/e2e/test_new.py
+++ tests/e2e/test_new.py
@@ -2,6 +2,7 @@ import os
 
 from typer.testing import CliRunner
 
+from src.app_creator.exceptions import NodeExistsError
 from cli.main import app
 
 
@@ -25,3 +26,14 @@ def test_creates_file_tree_in_cwd_if_no_path_passed(tmp_path):
 
     assert result.exit_code == 0
     assert (tmp_path / app_name).exists()
+
+
+def test_fails_when_directory_exists(tmp_path):
+    os.chdir(tmp_path)
+    app_name = 'users'
+
+    runner.invoke(app, ['new', 'app', app_name])
+    result = runner.invoke(app, ['new', 'app', app_name])
+
+    assert result.exit_code == 1
+    assert isinstance(result.exception, NodeExistsError)
